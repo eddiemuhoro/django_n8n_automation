e4364441a8e610718dc376fc1fa99508a5aa3605
eddiemuhoro <eddiemuhoro@gmail.com>
2025-05-18 00:17:28 +0300

    Add Project model to define project attributes and relationships

--- freelancer/models.py
+++ freelancer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    owner_id = models.BigIntegerField()
+    title = models.CharField(max_length=255)
+    url = models.URLField(max_length=200, blank=True, null=True)
+    description = models.TextField()
+    currency = models.CharField(max_length=10)
+    created = models.DateTimeField()
+    age_minutes = models.IntegerField()
+    bid_count = models.IntegerField()
+    bid_avg = models.FloatField()
+
+    def __str__(self):
+        return self.title
