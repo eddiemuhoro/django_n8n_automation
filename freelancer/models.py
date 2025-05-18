f3598664831587967f22034d6004bf162de49c8f
eddiemuhoro <eddiemuhoro@gmail.com>
2025-05-18 11:54:03 +0300

    Refactor Bid model to define bid attributes and relationships with Project

--- freelancer/models.py
+++ freelancer/models.py
@@ -15,3 +15,15 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Bid(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='bids')
+    project_id = models.BigIntegerField()
+    freelancer_id = models.BigIntegerField()
+    bid_amount = models.FloatField()
+    bid_message = models.TextField()
+    created = models.DateTimeField()
+
+    def __str__(self):
+        return f"Bid {self.id} for {self.project.title}"
